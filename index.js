3860ac769cd48a56ed055841a6e3842bac4e75c7
Abdirisak-Ahmed <khubaro2017@gmail.com>
2022-04-23 14:54:59 +0300

    LastCommit

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 // Create a port here and keep in mind you have to use locally and heroku
-const port = 4000;
- 
+const port  = process.env.PORT 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
